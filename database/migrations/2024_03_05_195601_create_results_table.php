d712727239b1c80f0c69827773b542f658061b4b
ma123@ <51268909+Ma1232@users.noreply.github.com>
2024-03-07 06:22:19 +0200

    Initial commit

---
+++ database/migrations/2024_03_05_195601_create_results_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('participant_id');
+            $table->unsignedBigInteger('event_id');
+            $table->integer('rank');
+            $table->foreign('participant_id')->references('id')->on('participants');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
